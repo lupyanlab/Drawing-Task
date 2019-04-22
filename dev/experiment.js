aaa519674348cc17f2be01fff09dd3550ded148a
Hettie Roebuck <hettieroebuck@Hetties-MacBook-Pro.local>
2019-04-22 12:53:43 -0500

    Update to task instructions

--- dev/experiment.js
+++ dev/experiment.js
@@ -63,7 +63,12 @@ function runExperiment(trials, subjCode, questions, workerId, assignmentId, hitI
     key_forward: "space",
     key_backward: "backspace",
     pages: [
-      `<p class="lead">Insert Instructions for IRQ
+      `<p class="lead">
+The following HIT will ask you a series of simple questions about language and mental imagery. 
+
+ First answer approx 40 quick multiple-choice questions about how you think.
+
+ Please think carefully about each question. If you answer carelessly, your payment may be denied.
             </p> ${continue_space}`
     ]
   };
@@ -100,7 +105,7 @@ function runExperiment(trials, subjCode, questions, workerId, assignmentId, hitI
     key_forward: "space",
     key_backward: "backspace",
     pages: [
-      `<p class="lead">Insert Instructions for first reading questions batch.
+      `<p class="lead">Following are statements about reading. For each statement, please decide what is most true for you select the extent to which you agree or disagree with each statement.
             </p> ${continue_space}`
     ]
   };
@@ -137,7 +142,7 @@ function runExperiment(trials, subjCode, questions, workerId, assignmentId, hitI
     key_forward: "space",
     key_backward: "backspace",
     pages: [
-      `<p class="lead">Insert Instructions for second reading questions batch.
+      `<p class="lead">
             </p> ${continue_space}`
     ]
   };
@@ -174,7 +179,7 @@ function runExperiment(trials, subjCode, questions, workerId, assignmentId, hitI
     type: "html-keyboard-response",
     choices: [32],
     stimulus: `<h1>Drawing</h1>
-        <p class="lead">Welcome to the experiment. Thank you for participating! Press SPACE to begin.</p>`
+        <p class="lead">For the next part of the task we would like you to draw some pictures. You will be given the name of something to draw, and then you will have a maximum of 60 seconds to complete your drawing from when you make your first mark. You will be asked to draw a total of 4 different pictures.</p>`
   };
 
   timeline.push(welcome_block);
@@ -184,7 +189,7 @@ function runExperiment(trials, subjCode, questions, workerId, assignmentId, hitI
     key_forward: "space",
     key_backward: "backspace",
     pages: [
-      `<p class="lead">Insert Instructions
+      `<p class="lead">When you are ready to start drawing press the space bar.
             </p> ${continue_space}`
     ]
   };
