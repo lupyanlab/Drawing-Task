a4bad833f7878a4d87c432bad3628206afadd041
Hettie Roebuck <hettieroebuck@Hetties-MacBook-Pro.local>
2019-07-16 11:46:53 -0500

    mturk version of superordinate drawing-task.
    Note. not to be directly replaced on the server with the current version running on SONA. This version has been created so the studies can run at the same time with a different link.

---
+++ mturk/README.md
@@ -0,0 +1,29 @@
+## Installation
+
+You must have nodjs installed: https://nodejs.org. Make sure Python 2 is installed.
+
+The following are instructions to download and install the repo.
+
+```sh
+git clone https://github.com/lupyanlab/Drawing-Task 
+cd Drawing-Task
+npm install
+pm2 start index.js --name Drawing-Task
+```
+
+## Development
+
+If you are working on your local machinese, go to http://localhost:7101.
+
+If you are working on Sapir, go to http://sapir.psych.wisc.edu/mturk/Drawing-Task/dev.
+
+The static HTML, CSS, and JavaScript files are in the `dev/` directory, and the Node.js API server is located in the root `./index.js` file.
+
+## Production
+
+When you are done, run the following command on Sapir and go to http://sapir.psych.wisc.edu/mturk/Drawing-Task/prod.
+
+```sh
+npm run prod
+```
+
---
+++ mturk/index.js
@@ -0,0 +1,269 @@
+// Dependencies
+const express = require('express');
+const path = require("path");
+const PythonShell = require('python-shell');
+const fs = require('fs');
+const csvWriter = require("csv-write-stream");
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+const csv = require('csvtojson');
+const jsonfile = require('jsonfile');
+
+let app = express();
+let writer = csvWriter({ sendHeaders: false });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('port', (process.env.PORT || 7101))
+
+// Add headers
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+// For Rendering HTML
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dev/index.html'));
+})
+app.use(express.static(__dirname + '/dev'));
+
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at http://localhost:" + app.get('port'))
+})
+
+// POST endpoint for requesting trials
+app.post('/trials', function (req, res, next) {
+  console.log("trials post request received");
+
+  let subjCode = req.body.subjCode;
+  let sessionId = req.body.sessionId;
+  console.log(req.body);
+
+  let trials = [];
+  fs.readdir('./trials', (err, filenames) => {
+    filenames = filenames.filter(filename =>
+      filename.startsWith("trials_drawing_")
+    );
+    let filename = filenames[Math.floor(Math.random()*filenames.length)];
+    csv({delimiter: ','})
+    .fromFile('./trials/'+filename)
+    .on('json',(jsonObj)=>{
+        trials.push(jsonObj);
+    })
+    .on('done',(error)=>{
+      trials.forEach((trial) => {
+        trial.file = filename;
+      });
+
+      // Parses to lines of questions to a shuffled array of questions
+      let questions = _.shuffle(fs.readFileSync('IRQ_questions.txt').toString().replace(/\r/g, '\n').split('\n')).filter((line) => {return line.replace(/ /g, '').length > 0 });
+      let ReadingQu1 = _.shuffle(fs.readFileSync('reading_questions_1.txt').toString().replace(/\r/g, '\n').split('\n').filter((line) => {return line.replace(/ /g, '').length > 0 }));
+      let ReadingQu2 = _.shuffle(fs.readFileSync('reading_questions_2.txt').toString().replace(/\r/g, '\n').split('\n').filter((line) => {return line.replace(/ /g, '').length > 0 }));
+
+      console.log(trials)
+      res.send({ success: true, trials: trials, questions, ReadingQu: [ReadingQu1, ReadingQu2] });
+    })
+  })
+})
+
+
+// POST endpoint for receiving trial responses
+app.post('/data', function (req, res, next) {
+  console.log('data post request received');
+
+  // Create new data folder if does not exist
+  let response = req.body;
+  fs.access('./data', (err) => {
+    if (err && err.code === 'ENOENT') {
+      fs.mkdir('./data', () => {
+        next();
+      });
+    }
+    else next();
+  });
+
+},
+  (req, res, next) => {
+    let response = req.body;
+    let path = 'data/' + response.workerId + '_data.json';
+    fs.access(path, (err) => {
+      if (err && err.code === 'ENOENT') {
+        jsonfile.writeFile(path, { trials: [] }, (err) => {
+          if (err) {
+            res.send({ success: false });
+            return next(err);
+          }
+          next();
+        })
+      }
+      else next();
+    })
+  },
+  (req, res, next) => {
+    // Write response to json
+    let response = req.body;
+    let path = 'data/' + response.workerId + '_data.json';
+    console.log(response);
+    jsonfile.readFile(path, (err, obj) => {
+      if (err) {
+        res.send({ success: false });
+        return next(err);
+      }
+      obj.trials.push(response);
+      jsonfile.writeFile(path, obj, (err) => {
+        if (err) {
+          res.send({ success: false });
+          return next(err);
+        }
+        res.send({ success: true });
+      })
+    })
+  });
+
+
+// POST endpoint for receiving demographics responses
+app.post('/demographics', function (req, res, next) {
+  let demographics = req.body;
+  console.log('demographics post request received');
+  console.log(demographics);
+  let path = 'demographics/' + demographics.subjCode + '_demographics.csv';
+
+  fs.access('./demographics', (err) => {
+    if (err && err.code === 'ENOENT') {
+      fs.mkdir('./demographics', () => {
+        next();
+      });
+    }
+    else next();
+  });
+
+},
+  (req, res, next) => {
+    let demographics = req.body;
+    let path = 'demographics/' + demographics.subjCode + '_demographics.csv';
+    fs.access(path, (err) => {
+      if (err && err.code === 'ENOENT') {
+        jsonfile.writeFile(path, { trials: [] }, (err) => {
+          if (err) {
+            res.send({ success: false });
+            return next(err);
+          }
+          next();
+        })
+      }
+      else next();
+    })
+  }, (req, res, next) => {
+    // Parses the trial response data to csv
+    let demographics = req.body;
+    let path = 'demographics/' + demographics.subjCode + '_demographics.csv';
+
+    writer = csvWriter({ headers: ['subjCode', 'question', 'response'] });
+
+    writer.pipe(fs.createWriteStream(path, { flags: 'w' }));
+    demographics.responses.forEach((response) => {
+      writer.write(response);
+    });
+    writer.end();
+
+    res.send({ success: true });
+  });
+
+
+// POST endpoint for receiving image responses
+app.post('/image', function (req, res, next) {
+  console.log('image post request received');
+
+  // Create new data folder if does not exist
+  let response = req.body;
+  fs.access('./data', (err) => {
+    if (err && err.code === 'ENOENT') {
+      fs.mkdir('./data', () => {
+        next();
+      });
+    }
+    else next();
+  });
+
+},
+  (req, res, next) => {
+    // Create new user folder if does not exist
+    let response = req.body;
+    fs.access('./data/' + response.workerId, (err) => {
+      if (err && err.code === 'ENOENT') {
+        fs.mkdir('./data/' + response.workerId, () => {
+          next();
+        });
+      }
+      else next();
+    });
+  },
+  (req, res, next) => {
+    // save as png
+    let response = req.body;
+    console.log(Object.keys(response))
+    var base64Data = response.drawing.replace(/^data:image\/png;base64,/, "");
+
+    fs.writeFile('./data/' + response.workerId+'/'+response.workerId+'_'+response.word_to_draw+'_'+response.trial_number+'.png', base64Data, 'base64', function (err) {
+      if (err) {
+        res.send({ success: false });
+        return next(err);
+      }
+      res.send({ success: true });
+    });
+  });
+
+  // POST endpoint for receiving trial responses
+app.post('/IRQ', function (req, res) {
+  console.log('IRQ post request received');
+
+  // Parses the trial response data to csv
+  let IRQ = req.body;
+  console.log(IRQ);
+  if (!fs.existsSync('IRQ')) {
+    fs.mkdirSync('IRQ');
+  }
+  let path = 'IRQ/' + IRQ.subjCode + '_IRQ.csv';
+  writer = csvWriter({ headers: ['subjCode', 'question', 'response'] });
+
+  writer.pipe(fs.createWriteStream(path, { flags: 'w' }));
+  IRQ.responses.forEach((response) => {
+    console.log(response);
+    writer.write(response);
+  })
+  writer.end();
+
+  res.send({ success: true });
+})
+
+// POST endpoint for receiving trial responses
+app.post('/ReadingQu', function (req, res) {
+  console.log('ReadingQu post request received');
+
+  // Parses the trial response data to csv
+  let ReadingQu = req.body;
+  console.log(ReadingQu);
+  if (!fs.existsSync('ReadingQu')) {
+    fs.mkdirSync('ReadingQu');
+  }
+  let path;
+  if (ReadingQu.batch === 1) {
+    path = 'ReadingQu/' + ReadingQu.subjCode + '_ReadingQu1.csv';
+  } else if (ReadingQu.batch === 2) {
+    path = 'ReadingQu/' + ReadingQu.subjCode + '_ReadingQu2.csv';
+  }
+  writer = csvWriter({ headers: ['subjCode', 'question', 'response'] });
+
+  writer.pipe(fs.createWriteStream(path, { flags: 'w' }));
+  ReadingQu.responses.forEach((response) => {
+    writer.write(response);
+  });
+  writer.end();
+
+  res.send({ success: true });
+})
