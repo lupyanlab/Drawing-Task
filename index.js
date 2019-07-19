adf7801cd8dbde5f001496b0cd964fc4e26396c9
Gary Lupyan <glupyan@gmail.com>
2019-07-19 14:33:07 -0500

    Updated port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let writer = csvWriter({ sendHeaders: false });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('port', (process.env.PORT || 7101))
+app.set('port', (process.env.PORT || 7151))
 
 // Add headers
 app.use(function (req, res, next) {
